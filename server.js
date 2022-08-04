138bce115353284e50590cb9a9c9d712efb6429f
0xDeviI <arminloveslinux@gmail.com>
2022-08-04 17:29:44 +0430

    main page setup

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// main file for the server
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('layouts/index', {
+        title: process.env.SITE_TITLE,
+        name: process.env.SITE_NAME
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
